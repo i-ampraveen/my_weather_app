cfcf0a5cd88e616e2a57152bcb2db190f6bf9fb2
Praveen Shivalingaswamy <praveen26mysore@gmail.com>
2021-10-27 20:55:04 +0530

    Static image rendering. Values are fetching. Need to do alignment, refinement, weather icon and image fetching dynamically.

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -14,11 +14,13 @@ class LocationScreen extends StatefulWidget {
 class _LocationScreenState extends State<LocationScreen> {
   WeatherModel weather = WeatherModel();
   late int temperature;
+  late double temp;
   late String city;
   late String weatherMessage;
   late Icon weatherIcon;
   late int airQuality;
   late int humidity;
+  late String airQualityText;
 
   @override
   void initState(){
@@ -26,20 +28,31 @@ class _LocationScreenState extends State<LocationScreen> {
     updateUI(widget.locationWeather);
   }
 
-  void updateUI(dynamic weatherData){
+  updateUI(dynamic weatherData){
     setState(() {
       if(weatherData == null){
-        temperature = 0;
+        double temp = 0.0;
         weatherMessage = 'Unable to get weather data';
         city = '';
         return;
       }
-      temperature = weatherData['current']['temp_c'].toDouble();
+      double temp = weatherData['current']['temp_c'].toDouble();
+      temperature = temp.toInt();
       city = weatherData['location']['name'];
       weatherMessage = weatherData['current']['condition']['text'];
-      weatherIcon = weatherData['current']['condition']['icon'];
+      //weatherIcon = weatherData['current']['condition']['icon'];
       airQuality = weatherData['current']['air_quality']['gb-defra-index'];
       humidity = weatherData['current']['humidity'];
+
+      if (airQuality>1 && airQuality<=3) {
+         airQualityText = 'Low';
+      } else if (airQuality>3 && airQuality<=6) {
+        airQualityText = 'Moderate';
+      } else if (airQuality>6 && airQuality<=9) {
+        airQualityText = 'Good';
+      } else if (airQuality>9) {
+        airQualityText = 'Very Good';
+      }
     });
   }
 
@@ -47,11 +60,61 @@ class _LocationScreenState extends State<LocationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage(''),
+              image: AssetImage('images/drizzle.jpg'),
               fit: BoxFit.cover,
           ),
+        ),
+        child: SafeArea(
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text('$temperature°',
+                        style: const TextStyle(
+                          fontSize: 100.0,
+                          color: Colors.white,
+                        ),
+                    ),
+                    const Text('C',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(city,
+                  style: const TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(weatherMessage,
+                  style: const TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                Text('Air Quality $airQualityText',
+                  style: const TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                Text('Humidity $humidity%',
+                  style: const TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
         ),
       ),
     );
